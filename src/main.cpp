7fb17bf09f5310af576fcca10db417132b551995
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 00:21:50 +0300

    Refactor main thread to use SupermanMain function
    Refactored the main thread function to improve control flow and registration.

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,14 @@
 
 static SupermanController g_superman;
 
-DWORD WINAPI MainThread(LPVOID)
+void SupermanMain()
 {
     bool lastF3 = false;
 
     while (true)
     {
-        Sleep(16);
-
         bool f3 = (GetAsyncKeyState(VK_F3) & 0x8000) != 0;
 
-        // Срабатывает один раз при нажатии F3.
         if (f3 && !lastF3)
         {
             g_superman.enabled = !g_superman.enabled;
@@ -28,33 +25,31 @@ DWORD WINAPI MainThread(LPVOID)
 
         lastF3 = f3;
 
-        // Пока Superman выключен, ничего не изменяем.
-        if (!g_superman.enabled)
-            continue;
+        if (g_superman.enabled)
+        {
+            g_superman.Tick(0.016f);
+        }
 
-        g_superman.Tick(0.016f);
+        // Передаём управление обратно GTA.
+        scriptWait(0);
     }
-
-    return 0;
 }
 
 BOOL APIENTRY DllMain(
     HMODULE hModule,
     DWORD reason,
-    LPVOID)
+    LPVOID
+)
 {
     if (reason == DLL_PROCESS_ATTACH)
     {
         DisableThreadLibraryCalls(hModule);
 
-        CreateThread(
-            nullptr,
-            0,
-            MainThread,
-            nullptr,
-            0,
-            nullptr
-        );
+        scriptRegister(hModule, SupermanMain);
+    }
+    else if (reason == DLL_PROCESS_DETACH)
+    {
+        scriptUnregister(hModule);
     }
 
     return TRUE;
