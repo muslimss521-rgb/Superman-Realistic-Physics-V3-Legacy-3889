f7de14f094dfe97d323140c51904f627572a8ae4
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-22 23:29:41 +0300

    Update Abilities.h

--- src/Abilities.h
+++ src/Abilities.h
@@ -1,25 +1,61 @@
 #pragma once
-#include "Physics.h"
 
-struct AbilityState {
-    bool flight=false, boost=false, superSpeed=false;
-    bool heatVision=false, freezeBreath=false, superBreath=false;
-    bool bulletTime=false, godMode=false;
-    bool grabbing=false;
+#include "Physics.h"
+#include <algorithm>
+
+struct AbilityState
+{
+    bool flight = false;
+    bool boost = false;
+    bool superSpeed = false;
+
+    bool heatVision = false;
+    bool freezeBreath = false;
+    bool superBreath = false;
+
+    bool bulletTime = false;
+    bool godMode = false;
+
+    bool grabbing = false;
 };
 
-class AbilitySystem {
+class AbilitySystem
+{
 public:
-    AbilityState state;
-    float heatPower=1.0f;
-    float freezeTemperature=-80.0f;
-    float breathForce=35.0f;
 
-    float HeatEnergy(float dt, float rangeFactor) const {
-        return heatPower * std::max(0.0f, dt) * std::max(0.0f, rangeFactor);
+    AbilityState state;
+
+    float heatPower = 1.0f;
+    float freezeTemperature = -80.0f;
+    float breathForce = 35.0f;
+
+    float HeatEnergy(float deltaTime, float rangeFactor) const
+    {
+        if (deltaTime < 0.0f)
+            deltaTime = 0.0f;
+
+        if (rangeFactor < 0.0f)
+            rangeFactor = 0.0f;
+
+        return heatPower * deltaTime * rangeFactor;
     }
 
-    float FreezeRate(float dt, float temperatureFactor) const {
-        return std::max(0.0f, dt) * std::max(0.0f, temperatureFactor);
+    float FreezeRate(float deltaTime, float temperatureFactor) const
+    {
+        if (deltaTime < 0.0f)
+            deltaTime = 0.0f;
+
+        if (temperatureFactor < 0.0f)
+            temperatureFactor = 0.0f;
+
+        return deltaTime * temperatureFactor;
+    }
+
+    float BreathForce(float distanceFactor) const
+    {
+        if (distanceFactor < 0.0f)
+            distanceFactor = 0.0f;
+
+        return breathForce * distanceFactor;
     }
 };
