78de9c2df5c65772b47df35853ad0c191fb53ca1
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 00:18:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <windows.h>
 #include "../include/ScriptHookV/main.h"
+#include "../include/ScriptHookV/natives.h"
 #include "Superman.h"
 
 static SupermanController g_superman;
 
 DWORD WINAPI MainThread(LPVOID)
 {
+    bool lastF3 = false;
+
     while (true)
     {
         Sleep(16);
 
-        // Обновляем физическое состояние Superman.
+        bool f3 = (GetAsyncKeyState(VK_F3) & 0x8000) != 0;
+
+        // Срабатывает один раз при нажатии F3.
+        if (f3 && !lastF3)
+        {
+            g_superman.enabled = !g_superman.enabled;
+
+            if (!g_superman.enabled)
+            {
+                g_superman.Reset();
+            }
+        }
+
+        lastF3 = f3;
+
+        // Пока Superman выключен, ничего не изменяем.
+        if (!g_superman.enabled)
+            continue;
+
         g_superman.Tick(0.016f);
     }
 
