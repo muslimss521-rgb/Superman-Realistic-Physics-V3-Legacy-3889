7492646779c91ebb28331f75050c641800b08bb0
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 00:36:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@
 
 static SupermanController g_superman;
 
+void ShowStatus()
+{
+    if (g_superman.enabled)
+    {
+        HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("STRING");
+        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME("SUPERMAN: ON");
+        HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(false, false);
+    }
+    else
+    {
+        HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("STRING");
+        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME("SUPERMAN: OFF");
+        HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(false, false);
+    }
+}
+
 void SupermanMain()
 {
     bool lastF3 = false;
@@ -21,6 +37,8 @@ void SupermanMain()
             {
                 g_superman.Reset();
             }
+
+            ShowStatus();
         }
 
         lastF3 = f3;
@@ -30,7 +48,6 @@ void SupermanMain()
             g_superman.Tick(0.016f);
         }
 
-        // Передаём управление обратно GTA.
         scriptWait(0);
     }
 }
@@ -38,13 +55,11 @@ void SupermanMain()
 BOOL APIENTRY DllMain(
     HMODULE hModule,
     DWORD reason,
-    LPVOID
-)
+    LPVOID)
 {
     if (reason == DLL_PROCESS_ATTACH)
     {
         DisableThreadLibraryCalls(hModule);
-
         scriptRegister(hModule, SupermanMain);
     }
     else if (reason == DLL_PROCESS_DETACH)
