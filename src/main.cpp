570714a14090b3d47b4545d15963a25cd1dc3c2a
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 09:11:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,42 +9,44 @@ static SupermanController g_superman;
 
 void SupermanMain()
 {
-    bool lastF5 = false;
+    bool lastF3 = false;
 
     while (true)
     {
-        bool f5 = (GetAsyncKeyState(VK_F5) & 0x8000) != 0;
+        bool f3 =
+            (GetAsyncKeyState(VK_F3) & 0x8000) != 0;
 
-        if (f5 && !lastF5)
+        if (f3 && !lastF3)
         {
-            g_superman.enabled = !g_superman.enabled;
+            g_superman.enabled =
+                !g_superman.enabled;
 
-            Ped ped = PLAYER::PLAYER_PED_ID();
-
-            if (g_superman.enabled)
-            {
-                g_superman.abilities.state.flight = true;
-                g_superman.abilities.state.superSpeed = true;
-                g_superman.abilities.state.godMode = true;
-                g_superman.abilities.state.heatVision = true;
-                g_superman.abilities.state.freezeBreath = true;
-                g_superman.abilities.state.superBreath = true;
-
-                ENTITY::SET_ENTITY_INVINCIBLE(ped, true);
-            }
-            else
-            {
-                g_superman.Reset();
-
-                ENTITY::SET_ENTITY_INVINCIBLE(ped, false);
-            }
+            g_superman.abilities.state.superSpeed =
+                g_superman.enabled;
         }
 
-        lastF5 = f5;
+        lastF3 = f3;
 
         if (g_superman.enabled)
         {
+            Ped ped = PLAYER::PLAYER_PED_ID();
+
+            // Увеличиваем скорость передвижения игрока
+            PED::SET_PED_MOVE_RATE_OVERRIDE(
+                ped,
+                2.0f
+            );
+
             g_superman.Tick(0.016f);
+        }
+        else
+        {
+            Ped ped = PLAYER::PLAYER_PED_ID();
+
+            PED::SET_PED_MOVE_RATE_OVERRIDE(
+                ped,
+                1.0f
+            );
         }
 
         scriptWait(0);
