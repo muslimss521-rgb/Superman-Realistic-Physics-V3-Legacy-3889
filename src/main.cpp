2962b610bba1ccf386384e7c5ae4092c33966c54
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 08:09:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,42 +7,17 @@
 
 static SupermanController g_superman;
 
-void ShowStatus()
-{
-    HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("STRING");
-
-    if (g_superman.enabled)
-    {
-        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(
-            "SUPERMAN: ON"
-        );
-    }
-    else
-    {
-        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(
-            "SUPERMAN: OFF"
-        );
-    }
-
-    HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(
-        false,
-        false
-    );
-}
-
 void SupermanMain()
 {
     bool lastF5 = false;
 
     while (true)
     {
-        bool f5 =
-            (GetAsyncKeyState(VK_F5) & 0x8000) != 0;
+        bool f5 = (GetAsyncKeyState(VK_F5) & 0x8000) != 0;
 
         if (f5 && !lastF5)
         {
-            g_superman.enabled =
-                !g_superman.enabled;
+            g_superman.enabled = !g_superman.enabled;
 
             if (g_superman.enabled)
             {
@@ -51,4 +26,40 @@ void SupermanMain()
                 g_superman.abilities.state.godMode = true;
                 g_superman.abilities.state.heatVision = true;
                 g_superman.abilities.state.freezeBreath = true;
-                g
+                g_superman.abilities.state.superBreath = true;
+                g_superman.abilities.state.boost = false;
+            }
+            else
+            {
+                g_superman.Reset();
+            }
+        }
+
+        lastF5 = f5;
+
+        if (g_superman.enabled)
+        {
+            g_superman.Tick(0.016f);
+        }
+
+        scriptWait(0);
+    }
+}
+
+BOOL APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD reason,
+    LPVOID)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hModule);
+        scriptRegister(hModule, SupermanMain);
+    }
+    else if (reason == DLL_PROCESS_DETACH)
+    {
+        scriptUnregister(hModule);
+    }
+
+    return TRUE;
+}
