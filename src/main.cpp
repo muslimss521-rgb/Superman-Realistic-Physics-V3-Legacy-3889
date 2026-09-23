351835fef76425e0392a1457da7e06952557437a
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-23 08:05:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,54 @@
-#include "ScriptHookV/main.h"
 #include <windows.h>
 
-static bool previousF5 = false;
+#include "../include/ScriptHookV/main.h"
+#include "../include/ScriptHookV/natives.h"
 
-static void Notify(const char* text)
+#include "Superman.h"
+
+static SupermanController g_superman;
+
+void ShowStatus()
 {
-    OutputDebugStringA("[SUPERMAN] ");
-    OutputDebugStringA(text);
-    OutputDebugStringA("\n");
+    HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("STRING");
+
+    if (g_superman.enabled)
+    {
+        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(
+            "SUPERMAN: ON"
+        );
+    }
+    else
+    {
+        HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(
+            "SUPERMAN: OFF"
+        );
+    }
+
+    HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(
+        false,
+        false
+    );
 }
 
-void main()
+void SupermanMain()
 {
-    Notify("SUPERMAN MAIN STARTED");
+    bool lastF5 = false;
 
     while (true)
     {
         bool f5 =
             (GetAsyncKeyState(VK_F5) & 0x8000) != 0;
 
-        if (f5 && !previousF5)
+        if (f5 && !lastF5)
         {
-            Notify("SUPERMAN F5 PRESSED");
-        }
+            g_superman.enabled =
+                !g_superman.enabled;
 
-        previousF5 = f5;
-
-        WAIT(0);
-    }
-}
+            if (g_superman.enabled)
+            {
+                g_superman.abilities.state.flight = true;
+                g_superman.abilities.state.superSpeed = true;
+                g_superman.abilities.state.godMode = true;
+                g_superman.abilities.state.heatVision = true;
+                g_superman.abilities.state.freezeBreath = true;
+                g
